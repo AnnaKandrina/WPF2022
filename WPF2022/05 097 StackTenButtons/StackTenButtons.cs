b1d6c4c6e228b7730ede776d986b41a4849856a0
AnnaKandrina <90605102+AnnaKandrina@users.noreply.github.com>
2023-06-27 03:03:29 +0500

    Datsyuk: 04 080, 05 097

---
+++ WPF2022/05 097 StackTenButtons/StackTenButtons.cs
@@ -0,0 +1,43 @@
+﻿//------------------------------------------------ 
+// StackTenButtons.cs (c) 2006 by Charles Petzold 
+//------------------------------------------------
+using System; 
+using System.Windows; 
+using System.Windows.Controls; 
+using System.Windows.Input;
+using System.Windows.Media; 
+namespace Petzold.StackTenButtons 
+{     
+    class StackTenButtons : Window     
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            Application app = new Application(); 
+            app.Run(new StackTenButtons());   
+        }        
+        public StackTenButtons()     
+        {            
+            Title = "Stack Ten Buttons";  
+            StackPanel stack = new StackPanel();
+            Content = stack;          
+            Random rand = new Random();  // создание объекта типа Random
+            for (int i = 0; i < 10; i++)   
+            {             
+                Button btn = new Button();     
+                btn.Name = ((char)('A' + i)) .ToString();       
+                btn.FontSize += rand.Next(10);  // увеличение свойства FontSize каждой кнопки на небольшую случайную величину
+
+                btn.Content = "Button " + btn .Name + " says 'Click me'";      
+                btn.Click += ButtonOnClick;             
+                stack.Children.Add(btn);  // кнопки включаются в коллекцию Children объекта  StackPanel       
+            }       
+        }     
+        void ButtonOnClick(object sender,  RoutedEventArgs args)   
+        {          
+            Button btn = args.Source as Button;   
+            MessageBox.Show("Button " + btn.Name +  " has been clicked",      
+                "Button Click");     
+        }  
+    }
+} 
