bb6426bef47bba1e8643257ac026abce2bc636ec
AnnaKandrina <90605102+AnnaKandrina@users.noreply.github.com>
2023-06-26 22:20:19 +0500

    Datsyuk: 02 037, 03 064

---
+++ WPF2022/02 037 VaryTheBackground/VaryTheBackground.cs
@@ -0,0 +1,45 @@
+﻿//-------------------------------------------------- 
+// VaryTheBackground.cs (c) 2006 by Charles Petzold 
+//--------------------------------------------------
+using System; 
+using System.Windows; 
+using System.Windows.Input; 
+using System.Windows.Media; 
+namespace Petzold.VaryTheBackground 
+{     public class VaryTheBackground : Window    // создан класс VaryTheBackground типа Window 
+    {         
+        SolidColorBrush brush = new  SolidColorBrush(Colors.Black); //изменение цвета кисти после создания объекта
+        [STAThread]         
+        public static void Main()         
+        {             
+            Application app = new Application(); // создание приложения
+            app.Run(new VaryTheBackground());   // открывает окно приложения      
+        }         
+        public VaryTheBackground()         
+        {             
+            Title = "Vary the Background";
+            Width = 384; 
+            Height = 384;    
+            Background = brush;
+        }         
+        protected override void OnMouseMove (MouseEventArgs args)
+        {             
+            double width = ActualWidth
+                - 2 * SystemParameters .ResizeFrameVerticalBorderWidth;   
+            double height = ActualHeight      
+                - 2 * SystemParameters .ResizeFrameHorizontalBorderHeight        
+                - SystemParameters.CaptionHeight;    
+            Point ptMouse = args.GetPosition(this);
+            Point ptCenter = new Point(width / 2,  height / 2);
+            Vector vectMouse = ptMouse - ptCenter;        
+            double angle = Math.Atan2(vectMouse.Y,  vectMouse.X);   
+            Vector vectEllipse = new Vector(width  / 2 * Math.Cos(angle),      
+                height  / 2 * Math.Sin(angle));    
+            Byte byLevel = (byte) (255 * (1 - Math .Min(1, vectMouse.Length /   
+                vectEllipse.Length)));       
+            Color clr = brush.Color;     
+            clr.R = clr.G = clr.B = byLevel;       
+            brush.Color = clr;        
+        }
+    }
+}
---
+++ WPF2022/03 064 ShapeAnEllipse/ShapeAnEllipse.cs
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------- 
+// ShapeAnEllipse.cs (c) 2006 by Charles Petzold 
+//-----------------------------------------------
+using System; 
+using System.Windows; 
+using System.Windows.Controls; 
+using System.Windows.Input; 
+using System.Windows.Media; 
+using System.Windows.Shapes; 
+namespace Petzold.ShapeAnEllipse 
+{     class ShapeAnEllipse : Window  //класс, производный от Window 
+    {         
+        [STAThread]  //используется однопоточная модель
+        public static void Main()         
+        {             
+            Application app = new Application();  //создаем объект типа Application
+            app.Run(new ShapeAnEllipse());  //вызов метода Run, котрый запускает цикл сообщений
+        }         
+        public ShapeAnEllipse()         
+        {             
+            Title = "Shape an Ellipse";  //определение текста заголовка окна
+            Ellipse elips = new Ellipse(); //создан элемент Ellipse
+            elips.Fill = Brushes.AliceBlue; //внутренная часть эллипса закрашивается бледно-голубым цветом
+            elips.StrokeThickness = 24;     //граница имеет ширину 1/4  дюйма
+            elips.Stroke =                 
+                new LinearGradientBrush(Colors .CadetBlue, Colors.Chocolate, 
+                                        new Point (1, 0), new Point(0, 1));      
+            Content = elips;       
+        }     
+    } 
+} 
